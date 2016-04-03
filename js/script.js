746401a2898805bcdabd6f35158ac4caba24f050
laurenfromseattle <l.blue.tucker@gmail.com>
2016-04-03 15:38:37 -0700

    Load background image

--- js/script.js
+++ js/script.js
@@ -12,10 +12,20 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ', ' + city;
+    var apiKey = 'AIzaSyDgTpc54IsE0AIK5EHJbInToW3YeJvZLx0';
+    var size = '600x400';
+    var imageSrc = 'https://maps.googleapis.com/maps/api/streetview?' +
+        'location=' + address +
+        '&size=' + size +
+        '&key=' + apiKey;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text('So you want to live at ' + street + ' in ' + city + '?');
+    $body.append('<img class="bgimg" src="' + imageSrc + '" />');
 
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
